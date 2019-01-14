aebbff239fb9b19eb832bfbf494267e3e37d40ab
manpreetrai1912 <46692917+manpreetrai1912@users.noreply.github.com>
2019-01-14 17:04:44 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,20 @@
+// name:manpreet kaur
+//date:jan 14-2018
+//student:c0742302
+//coursecode:CSD 2214 SECTION6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+
+
+console.log("manpreet kaur");
+console.log(endDate.value);
+
+
